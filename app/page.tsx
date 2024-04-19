4621c4e5538e88f2c679358764db9f344d5a7569
Adrian Wika <adrian.wika@marketdino.pl>
2024-04-19 11:16:29 +0200

    feat: :arrow_up: update project & init new one

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import CircleParticles from '../components/background/ParticlesBackground'
-export default function Home() {
-  return (
-    <main>
-      <CircleParticles />
-    </main>
-  )
-}
