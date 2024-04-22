d59d9dbf54eee5628623236043bdccd2442ff52a
Adrian Wika <adrian.wika@marketdino.pl>
2024-04-22 13:52:39 +0200

    feat: points i Z axis move

--- components/canvas_test/mainCanvas.js
+++ components/canvas_test/mainCanvas.js
@@ -1,22 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import * as THREE from 'three'
-
-function moveInsideCircle(renderer, currentPosition, targetPosition, setCurrentPosition) {
-    if (currentPosition > targetPosition) {
-        setTimeout(() => {
-            setCurrentPosition(currentPosition - 2)
-        }, 25)
-    } else {
-        setTimeout(() => {
-            window.location.reload()
-        }, 1000)
-
-    }
-
-}
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
 
 const MainCanvas = () => {
-    const [currentPosition, setCurrentPosition] = useState(30)
 
     useEffect(() => {
         if (typeof window !== "undefined") {
@@ -28,22 +15,71 @@ const MainCanvas = () => {
 
             renderer.setPixelRatio(window.devicePixelRatio)
             renderer.setSize(window.innerWidth, window.innerHeight)
-            camera.position.setZ(currentPosition)
+            camera.position.setZ(10)
+            camera.position.setY(1)
             renderer.render(scene, camera)
-            // moveInsideCircle(renderer, currentPosition, 0, setCurrentPosition)
-            const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-            const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
-            const torus = new THREE.Mesh(geometry, material)
 
-            scene.add(torus)
+            const pointLight = new THREE.PointLight(0xffffff)
+            const ambientLight = new THREE.AmbientLight(0xffffff)
+            const lightHelper = new THREE.PointLightHelper(pointLight)
+            const gridHelper = new THREE.GridHelper(200, 50)
+            const axisHelper = new THREE.AxesHelper(60)
+
+            scene.add(ambientLight)
+            scene.add(pointLight)
+            scene.add(lightHelper, gridHelper, axisHelper)
+            const controls = new OrbitControls(camera, renderer.domElement)
+
+            const particlesMaterial = new THREE.PointsMaterial({
+                color: 0xffffff,
+                size: 0.25
+            })
+            const particlesGeometry = new THREE.BufferGeometry()
+            const particlesCount = 100
+            const vertices = []
+            const velocities = []
+            const accelerations = []
+
+            for (let index = 0; index < particlesCount; index++) {
+                const particle = new THREE.Vector3(
+                    index,
+                    1,
+                    1
+                )
+                vertices.push(...particle)
+                velocities.push(0.005)
+                accelerations.push(1)
+            }
+
+            particlesGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3))
+            particlesGeometry.center()
+            const particleMesh = new THREE.Points(particlesGeometry, particlesMaterial)
+
+            scene.add(particleMesh)
+
 
             function animate() {
                 requestAnimationFrame(animate)
 
-                torus.rotateX(0.01)
-                torus.rotateY(0.005)
-                torus.rotateZ(0.01)
+                const positionAttribute = particlesGeometry.getAttribute('position')
 
+                for (let i = 0; i < positionAttribute.count; i++) {
+
+                    let z = positionAttribute.getZ(i)
+
+                    let vel = velocities[i]
+                    let accel = accelerations[i]
+                    vel *= accel
+                    velocities[i] = vel
+                    z -= vel
+
+                    positionAttribute.setZ(i, z)
+
+                }
+
+                positionAttribute.needsUpdate = true
+
+                controls.update()
                 renderer.render(scene, camera)
 
             }
