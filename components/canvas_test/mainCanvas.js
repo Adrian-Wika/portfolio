16d539582c448b2a5a7050956a760124e5b2e62e
Adrian Wika <adrian.wika@marketdino.pl>
2024-04-23 10:30:43 +0200

    feat: :sparkles: text to particles font

--- components/canvas_test/mainCanvas.js
+++ components/canvas_test/mainCanvas.js
@@ -1,9 +1,76 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
+import { FontLoader } from 'three/addons/loaders/FontLoader.js'
+
+function createText(font, materials, scene) {
+    const thePoints = []
+    const shapes = font.generateShapes('TEST', 20)
+    const geometry = new THREE.ShapeGeometry(shapes)
+    geometry.computeBoundingBox()
+    const xMid = - 0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x)
+    const yMid = (geometry.boundingBox.max.y - geometry.boundingBox.min.y) / 2.85
+    let holeShapes = []
+
+    for (let q = 0; q < shapes.length; q++) {
+
+        let shape = shapes[q]
+
+        if (shape.holes && shape.holes.length > 0) {
+
+            for (let j = 0; j < shape.holes.length; j++) {
+
+                let hole = shape.holes[j]
+                holeShapes.push(hole)
+            }
+        }
+
+    }
+
+    shapes.push.apply(shapes, holeShapes)
+
+    const sizes = []
+
+    for (let x = 0; x < shapes.length; x++) {
+
+        let shape = shapes[x]
+
+        const amountPoints = (shape.type == 'Path') ? 1500 / 2 : 1500
+
+        const points = shape.getSpacedPoints(amountPoints)
+
+        points.forEach((element, z) => {
+
+            const a = new THREE.Vector3(element.x, element.y, 0)
+            thePoints.push(a)
+            sizes.push(1)
+
+        })
+    }
+
+    const geoParticles = new THREE.BufferGeometry().setFromPoints(thePoints)
+    geoParticles.translate(xMid, yMid, 0)
+    geoParticles.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1))
+
+    const material = new THREE.PointsMaterial({ color: 0xffffff })
+
+    const particles = new THREE.Points(geoParticles, material)
+    scene.add(particles)
+
+}
 
 const MainCanvas = () => {
+    const [font, setFont] = useState(undefined)
+
+    useEffect(() => {
+        const loader = new FontLoader()
+
+        loader.load('https://res.cloudinary.com/dydre7amr/raw/upload/v1612950355/font_zsd4dr.json', (result) => {
+            setFont(result)
+        })
+
+    }, [])
+
 
     useEffect(() => {
         if (typeof window !== "undefined") {
@@ -15,8 +82,8 @@ const MainCanvas = () => {
 
             renderer.setPixelRatio(window.devicePixelRatio)
             renderer.setSize(window.innerWidth, window.innerHeight)
-            camera.position.setZ(10)
-            camera.position.setY(1)
+            camera.position.setZ(50)
+            camera.position.setY(30)
             renderer.render(scene, camera)
 
             const pointLight = new THREE.PointLight(0xffffff)
@@ -30,54 +97,14 @@ const MainCanvas = () => {
             scene.add(lightHelper, gridHelper, axisHelper)
             const controls = new OrbitControls(camera, renderer.domElement)
 
-            const particlesMaterial = new THREE.PointsMaterial({
-                color: 0xffffff,
-                size: 0.25
-            })
-            const particlesGeometry = new THREE.BufferGeometry()
-            const particlesCount = 100
-            const vertices = []
-            const velocities = []
-            const accelerations = []
+            const materials = new THREE.PointsMaterial({ color: 0xffffff })
 
-            for (let index = 0; index < particlesCount; index++) {
-                const particle = new THREE.Vector3(
-                    index,
-                    1,
-                    1
-                )
-                vertices.push(...particle)
-                velocities.push(0.005)
-                accelerations.push(1)
+            if (font) {
+                createText(font, materials, scene)
             }
-
-            particlesGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3))
-            particlesGeometry.center()
-            const particleMesh = new THREE.Points(particlesGeometry, particlesMaterial)
-
-            scene.add(particleMesh)
-
 
             function animate() {
                 requestAnimationFrame(animate)
-
-                const positionAttribute = particlesGeometry.getAttribute('position')
-
-                for (let i = 0; i < positionAttribute.count; i++) {
-
-                    let z = positionAttribute.getZ(i)
-
-                    let vel = velocities[i]
-                    let accel = accelerations[i]
-                    vel *= accel
-                    velocities[i] = vel
-                    z -= vel
-
-                    positionAttribute.setZ(i, z)
-
-                }
-
-                positionAttribute.needsUpdate = true
 
                 controls.update()
                 renderer.render(scene, camera)
@@ -86,7 +113,7 @@ const MainCanvas = () => {
 
             animate()
         }
-    }, [])
+    }, [font])
 
 
     return (
