29d6f3422467aefbc362115ceed831687a43e8bb
Adrian Wika <awika157@gmail.com>
2024-06-04 22:07:59 +0200

    style: :lipstick: change in ui

--- components/content/technologies/TechnologiesManager.tsx
+++ components/content/technologies/TechnologiesManager.tsx
@@ -4,7 +4,7 @@ const TechnologiesManager = () => {
     return (
         <div className="mt-[1vh]">
             <div className="bg-transparent backdrop-blur-md p-5 rounded-lg shadow-lg max-w-lg text-center">
-                <h1 className="text-2xl md:text-5xl font-bold mb-4">Mój zakres technologiczny:</h1>
+                <h1 className="text-2xl md:text-5xl font-bold mb-4">Technologie z którymi pracowałem:</h1>
                 <div>
                     honeycomb z ikonami technologi
                 </div>
