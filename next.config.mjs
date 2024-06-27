bae67725fa642ce305c872bb62997bf864b73766
Adrian Wika <awika157@gmail.com>
2024-06-27 23:03:51 +0200

    fix: :bug: trying to debug three js prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  productionBrowserSourceMaps: true,
   swcMinify: false,
   webpack: (config) => {
     config.module.rules.push({
