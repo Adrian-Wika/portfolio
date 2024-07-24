b2bb24d6224a10a7e4fb8938e1bbb5d6b2e89bd6
Adrian Wika <awika157@gmail.com>
2024-07-24 21:42:33 +0200

    fix: :bug: fixed zoom bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ export default function IndexPage() {
         </div>
       </div>
 
-      <div id='bg' className='!absolute !top-0 !left-0 !z-[-10] !h-[100%] gradient-background'>
+      <div id='bg' className='!absolute !top-0 !left-0 !z-[-10] gradient-background'>
         <MainCanvas />
       </div>
     </div>
